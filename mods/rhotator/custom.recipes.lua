6fe844f46bbf0312314400bdc5e7836cd3106621
Singularis <singularis@volny.cz>
2022-07-05 12:00:26 +0200

    [rhotator]
    - lokalizace
    - odstraněn multifunkční rotátor
    - paměťový rotátor pracuje, jen když je v hlavním inventáři
    - odstraněna chyba projevující se při stavbě digtronem (paměťový rotátor
    se při stavbě digtronem nemá uplatnit)

---
+++ mods/rhotator/custom.recipes.lua
@@ -0,0 +1,50 @@
+-- only alter this file if it's named "custom.recipes.lua"
+-- alter the recipes as you please and delete / comment out
+-- the recipes you don't want to be available in the game
+-- the original versions are in "default/recipes.lua"
+
+return {
+    {
+        output = "rhotator:screwdriver",
+        recipe = {
+            {"default:copper_ingot"},
+            {"group:stick"},
+        },
+    },
+    {
+        output = "rhotator:screwdriver_alt",
+        recipe = {
+            {"default:copper_ingot", "group:stick"},
+            {"group:stick", ""},
+        },
+    },
+    {
+        output = "rhotator:memory",
+        recipe = {
+            {"group:stick"},
+            {"default:copper_ingot"},
+            {"group:stick"},
+        },
+    },
+    {
+        output = "rhotator:cube",
+        recipe = {
+            {"group:wool"},
+            {"rhotator:screwdriver"},
+        },
+    },
+    {
+        output = "rhotator:cube",
+        recipe = {
+            {"group:wool"},
+            {"rhotator:screwdriver_alt"},
+        },
+    },
+    {
+        output = "rhotator:cube",
+        recipe = {
+            {"group:wool"},
+            {"rhotator:memory"},
+        },
+    },
+}
