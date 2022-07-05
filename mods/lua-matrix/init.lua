b38c08be68a10e458925f5d40664103cd85abdc7
Singularis <singularis@volny.cz>
2022-07-05 11:13:23 +0200

    [lua_matrix]

--- mods/lua-matrix/init.lua
+++ mods/lua-matrix/init.lua
@@ -1,8 +1,11 @@
-
 -- this whole mod is just a thin wrapper around https://github.com/davidm/lua-matrix
+
+print("[MOD BEGIN] " .. minetest.get_current_modname() .. "(" .. os.clock() .. ")")
 
 local mod_path = minetest.get_modpath(minetest.get_current_modname())
 
 matrix = dofile(mod_path .. "/matrix.lua")
 
 -- that's it
+
+print("[MOD BEGIN] " .. minetest.get_current_modname() .. "(" .. os.clock() .. ")")
