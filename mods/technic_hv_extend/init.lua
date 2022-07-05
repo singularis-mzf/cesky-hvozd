13195afd36d27df65f063b3ff8e3eddd13e2899b
Singularis <singularis@volny.cz>
2022-07-05 12:32:54 +0200

    [technic_hv_extend]

--- mods/technic_hv_extend/init.lua
+++ mods/technic_hv_extend/init.lua
@@ -1,6 +1,8 @@
+print("[MOD BEGIN] " .. minetest.get_current_modname() .. "(" .. os.clock() .. ")")
 local modname = minetest.get_current_modname()
 local path = minetest.get_modpath(modname)
 
 dofile(path.."/electric_furnace.lua")
 dofile(path.."/grinder.lua")
 dofile(path.."/compressor.lua")
+print("[MOD END] " .. minetest.get_current_modname() .. "(" .. os.clock() .. ")")
