86bb09ba6c7fb7f3ca735e146f96cf0f13ac5be0
Singularis <singularis@volny.cz>
2022-07-17 12:43:25 +0200

    [technic_cnc] vyjmutí opracované střešní krytiny z palety předmětů
    - odebrán [mobs_bat] (v plánu je import lepších netopýrů)

--- mods/technic/technic_cnc/materials/cottages.lua
+++ mods/technic/technic_cnc/materials/cottages.lua
@@ -1,6 +1,6 @@
 -- local S = technic_cnc.getter
 
-technic_cnc.register_all("cottages:reet", {hay = 3, snappy=3,choppy=3,oddly_breakable_by_hand=3,flammable=3}, {"cottages_reet.png"}, "Špinavá sláma")
-technic_cnc.register_all("cottages:black", {snappy=2,choppy=2,oddly_breakable_by_hand=2}, {"cottages_homedecor_shingles_asphalt.png"}, "Asfaltová střešní krytina")
-technic_cnc.register_all("cottages:red", {snappy=2,choppy=2,oddly_breakable_by_hand=2}, {"cottages_homedecor_shingles_terracotta.png"}, "Světlé křidlice (střešní krytina)")
-technic_cnc.register_all("cottages:brown", {snappy=2,choppy=2,oddly_breakable_by_hand=2}, {"cottages_homedecor_shingles_wood.png"}, "Tmavé křidlice (střešní krytina)")
+technic_cnc.register_all("cottages:reet", {hay = 3, snappy=3,choppy=3,oddly_breakable_by_hand=3,flammable=3,not_in_creative_inventory=1}, {"cottages_reet.png"}, "Špinavá sláma")
+technic_cnc.register_all("cottages:black", {snappy=2,choppy=2,oddly_breakable_by_hand=2,not_in_creative_inventory=1}, {"cottages_homedecor_shingles_asphalt.png"}, "Asfaltová střešní krytina")
+technic_cnc.register_all("cottages:red", {snappy=2,choppy=2,oddly_breakable_by_hand=2,not_in_creative_inventory=1}, {"cottages_homedecor_shingles_terracotta.png"}, "Světlé křidlice (střešní krytina)")
+technic_cnc.register_all("cottages:brown", {snappy=2,choppy=2,oddly_breakable_by_hand=2,not_in_creative_inventory=1}, {"cottages_homedecor_shingles_wood.png"}, "Tmavé křidlice (střešní krytina)")
