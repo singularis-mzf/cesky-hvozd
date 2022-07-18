e2784ff3c68c3f35823fea3dc36b6cae189ee62d
Singularis <singularis@volny.cz>
2022-07-18 19:02:28 +0200

    [ch_core] oprava nedostatečně ošetřené funkce
    - [technic] Kapesní svítilna přejmenována na baterku

--- mods/ch_core/hotbar.lua
+++ mods/ch_core/hotbar.lua
@@ -1,36 +1,43 @@
 function ch_core.predmety_na_liste(player, jako_pole)
 	local result = {}
-	if player then
-		local inv = player:get_inventory()
-		local hotbar = inv:get_list("main")
-		local hotbar_length = player:hud_get_hotbar_itemcount()
+	if not player then
+		return result
+	end
+	local inv = player:get_inventory()
+	if not inv then
+		return result
+	end
+	local hotbar = inv:get_list("main")
+	if not hotbar then
+		return result
+	end
+	local hotbar_length = player:hud_get_hotbar_itemcount()
 
-		if not hotbar_length then
-			hotbar_length = 8
-		elseif hotbar_length > 32 then
-			hotbar_length = 32
-		elseif hotbar_length < 1 then
-			hotbar_length = 1
-		end
+	if not hotbar_length then
+		hotbar_length = 8
+	elseif hotbar_length > 32 then
+		hotbar_length = 32
+	elseif hotbar_length < 1 then
+		hotbar_length = 1
+	end
 
-		if jako_pole then
-			for i = 1, hotbar_length, 1 do
-				local name = hotbar[i]:get_name()
-				if name and name ~= "" then
-					local t = result[name]
-					if t then
-						table.insert(t, i)
-					else
-						result[name] = {i}
-					end
+	if jako_pole then
+		for i = 1, hotbar_length, 1 do
+			local name = hotbar[i]:get_name()
+			if name and name ~= "" then
+				local t = result[name]
+				if t then
+					table.insert(t, i)
+				else
+					result[name] = {i}
 				end
 			end
-		else
-			for i = hotbar_length, 1, -1 do
-				local name = hotbar[i]:get_name()
-				if name and name ~= "" then
-					result[name] = i
-				end
+		end
+	else
+		for i = hotbar_length, 1, -1 do
+			local name = hotbar[i]:get_name()
+			if name and name ~= "" then
+				result[name] = i
 			end
 		end
 	end
