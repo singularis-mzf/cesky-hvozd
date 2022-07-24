b2be3533340eed95f35a5ff5607283be697a1f6f
Singularis <singularis@volny.cz>
2022-07-24 19:34:36 +0200

    [mask] adaptace na clothing a lokalizace

--- mods/mask/init.lua
+++ mods/mask/init.lua
@@ -1,20 +1,13 @@
-
-
 ---
 ---Mask
 ---
 
-if minetest.get_modpath("3d_armor") then
-  
-  armor:register_armor("mask:mask", {
-		description = ("Mask"),
-		inventory_image = "mask_inv_mask.png",
-		groups = {armor_head=1, armor_heal=12, armor_use=10, armor_fire=1},
-		armor_groups = {fleshy=15},
-		damage_groups = {cracky=2, snappy=1, level=3},
-	})
-end
-
+minetest.register_craftitem("mask:mask", {
+	description = "rouška",
+	inventory_image = "mask_inv_mask.png",
+	uv_image = "mask_mask.png",
+	groups = {clothing = 1},
+})
 
 ---
 ---Craft
@@ -23,8 +16,54 @@ end
 minetest.register_craft({
 	output = "mask:mask",
 	recipe = {
-		{"", "", ""},
-		{"farming:string", "default:paper", "farming:string"},
-		{"", "", ""},
-	}
+		{"default:paper", "clothing:yarn_spool_white"},
+		{"", ""},
+	},
+	replacements = {
+		{"clothing:yarn_spool_white", "clothing:yarn_spool_empty"},
+	},
+})
+
+clothing.sewing_table:recipe_register_input("", {
+	outputs = {{"mask:mask 2", "clothing:yarn_spool_empty"}},
+	inputs = {
+		"default:paper", "clothing:yarn_spool_white", "default:paper",
+		"", "", "",
+		"", "", "",
+	},
+	production_time = 10,
+	consumption_step_size = 1,
+})
+
+clothing.sewing_table:recipe_register_input("", {
+	outputs = {{"mask:mask 4", "clothing:yarn_spool_empty"}},
+	inputs = {
+		"default:paper", "clothing:yarn_spool_white", "default:paper",
+		"default:paper", "", "default:paper",
+		"", "", "",
+	},
+	production_time = 20,
+	consumption_step_size = 1,
+})
+
+clothing.sewing_table:recipe_register_input("", {
+	outputs = {{"mask:mask 7", "clothing:yarn_spool_empty"}},
+	inputs = {
+		"default:paper", "clothing:yarn_spool_white", "default:paper",
+		"default:paper", "", "default:paper",
+		"default:paper", "", "default:paper",
+	},
+	production_time = 30,
+	consumption_step_size = 1,
+})
+
+clothing.sewing_table:recipe_register_input("", {
+	outputs = {{"mask:mask 10", "clothing:yarn_spool_empty"}},
+	inputs = {
+		"default:paper", "clothing:yarn_spool_white", "default:paper",
+		"default:paper", "default:paper", "default:paper",
+		"default:paper", "default:paper", "default:paper",
+	},
+	production_time = 40,
+	consumption_step_size = 1,
 })
