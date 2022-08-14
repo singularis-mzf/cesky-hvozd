abb98791f5142ecd1a669cc34a788bb0e44a29c5
Singularis <singularis@volny.cz>
2022-08-14 20:15:00 +0200

    [bushy_leaves] pokus o úsporný model pro často používané typy listů a jehličí

--- mods/bushy_leaves/init.lua
+++ mods/bushy_leaves/init.lua
@@ -17,45 +17,64 @@ Codezeilen und schreibe das auch nicht dran.
 
 print("[MOD BEGIN] " .. minetest.get_current_modname() .. "(" .. os.clock() .. ")")
 
-local leaves_nodes = {
-	"default:acacia_bush_leaves",
-	"default:acacia_leaves",
-	"default:aspen_leaves",
-	"default:blueberry_bush_leaves",
-	"default:blueberry_bush_leaves_with_berries",
-	"default:bush_leaves",
-	"default:leaves",
-	"ebony:leaves",
-	"cherrytree:blossom_leaves",
-	"cherrytree:leaves",
-	"chestnuttree:leaves",
-	"plumtree:leaves",
-	"moretrees:apple_tree_leaves",
-	"moretrees:beech_leaves",
-	"moretrees:birch_leaves",
-	"moretrees:cedar_leaves",
-	"moretrees:date_palm_leaves",
-	"moretrees:fir_leaves",
-	"moretrees:fir_leaves_bright",
-	"moretrees:oak_leaves",
-	"moretrees:palm_leaves",
-	"moretrees:poplar_leaves",
-	"moretrees:rubber_tree_leaves",
-	"moretrees:sequoia_leaves",
-	"moretrees:spruce_leaves",
-	"moretrees:willow_leaves",
-	"willow:leaves",
-	-- "default:jungleleaves",
-	-- "moretrees:jungletree_leaves_yellow",
-	-- "moretrees:jungletree_leaves_red",
-	-- "default:pine_bush_needles",
-	-- "default:pine_needles",
-}
+-- 0 => do not change
+-- 1 => simple model
+-- 2 => full bushy model
 
--- local mods_with_leaves = {
-    -- ["default"] = 1,
-    -- ["moretrees"] = 1,
--- }
+local leaves_nodes = {
+	-- Cool Trees
+	["baldcypress:leaves"] = 1,
+	["bamboo:leaves"] = 1,
+	["birch:leaves"] = 1,
+	["cacaotree:leaves"] = 1,
+	["clementinetree:leaves"] = 1,
+	["ebony:leaves"] = 1,
+	["hollytree:leaves"] = 1,
+	["cherrytree:leaves"] = 2,
+	["chestnuttree:leaves"] = 1,
+	["jacaranda:blossom_leaves"] = 1,
+	["larch:leaves"] = 1,
+	["lemontree:leaves"] = 1,
+	["mahogany:leaves"] = 1,
+	["maple:leaves"] = 1,
+	["oak:leaves"] = 1,
+	["palm:leaves"] = 1,
+	["plumtree:leaves"] = 1,
+	["pomegranate:leaves"] = 1,
+	["sequoia:leaves"] = 1,
+	["willow:leaves"] = 1,
+
+	-- More Trees
+	["moretrees:apple_tree_leaves"] = 2,
+	["moretrees:beech_leaves"] = 1,
+	["moretrees:birch_leaves"] = 1,
+	["moretrees:cedar_leaves"] = 1,
+	["moretrees:date_palm_leaves"] = 1,
+	["moretrees:fir_leaves"] = 1,
+	["moretrees:fir_leaves_bright"] = 1,
+	["moretrees:oak_leaves"] = 1,
+	["moretrees:palm_leaves"] = 1,
+	["moretrees:poplar_leaves"] = 1,
+	["moretrees:sequoia_leaves"] = 1,
+	["moretrees:spruce_leaves"] = 1,
+	["moretrees:willow_leaves"] = 1,
+	["moretrees:rubber_tree_leaves"] = 1,
+	["moretrees:jungletree_leaves_red"] = 1,
+	["moretrees:jungletree_leaves_yellow"] = 1,
+
+	-- default
+	["default:acacia_bush_leaves"] = 1,
+	["default:acacia_leaves"] = 1,
+	["default:aspen_leaves"] = 1,
+	["default:blueberry_bush_leaves"] = 1,
+	["default:blueberry_bush_leaves_with_berries"] = 2,
+	["default:bush_leaves"] = 1,
+	["default:leaves"] = 1,
+
+	["default:jungleleaves"] = 1,
+	["default:pine_bush_needles"] = 1,
+	["default:pine_needles"] = 1,
+}
 
 local node_box_bushy_leaves = {
 	type = "fixed",
@@ -74,18 +93,70 @@ local node_box_bushy_leaves = {
 	},
 }
 
+local node_box_bushy_leaves_low_budget = {
+	type = "fixed",
+	fixed = {
+		{  0/16, -4/16,-12/16,  0/16,  4/16, 12/16 },
+		{  0/16, -8/16, -8/16,  0/16,  8/16,  8/16 },
+		-- {  0/16,-12/16, -4/16,  0/16, 12/16,  4/16 },
+
+		{ -8/16,  0/16, -8/16,  8/16,  0/16,  8/16 },
+
+		{-12/16, -4/16,  0/16, 12/16,  4/16,  0/16 },
+		{ -8/16, -8/16,  0/16,  8/16,  8/16,  0/16 },
+		-- { -4/16,-12/16,  0/16,  4/16, 12/16,  0/16 },
+	},
+}
+
 local node_box_full_node = {
 	type = "fixed",
 	fixed = { -8/16, -8/16, -8/16,  8/16,  8/16,  8/16 },
 }
 
+--[[
 local bushy_leaves_override = {
 	drawtype = "nodebox",
 	node_box = node_box_bushy_leaves,
 	collision_box = node_box_full_node,
-	use_texture_alpha = "blend",
+	use_texture_alpha = "clip",
+} ]]
+
+local override_0 = {
+	climbable = true,
+	walkable = false,
+}
+local override_1 = {
+	climbable = true,
+	walkable = false,
+	drawtype = "nodebox",
+	node_box = node_box_bushy_leaves_low_budget,
+-- 	collision_box = node_box_full_node,
+	use_texture_alpha = "clip",
+}
+local override_2 = {
+	climbable = true,
+	walkable = false,
+	drawtype = "nodebox",
+	node_box = node_box_bushy_leaves,
+--	collision_box = node_box_full_node,
+	use_texture_alpha = "clip",
 }
 
+for name, level in pairs(leaves_nodes) do
+	local def = minetest.registered_nodes[name]
+	if def then
+		def.walkable = false
+		if level == 2 then
+			minetest.override_item(name, override_2)
+		elseif level == 1 then
+			minetest.override_item(name, override_1)
+		else
+			minetest.override_item(name, override_0)
+		end
+	end
+end
+
+--[[
 local add_bushy_leaves = function()
 	-- local counter = 0
 	for i, v in ipairs(leaves_nodes) do
@@ -97,4 +168,5 @@ local add_bushy_leaves = function()
 end
 
 minetest.register_on_mods_loaded(add_bushy_leaves)
+]]
 print("[MOD END] " .. minetest.get_current_modname() .. "(" .. os.clock() .. ")")
