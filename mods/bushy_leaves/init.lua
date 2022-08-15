1e5c59cb5edf8d7c639abbc2cdc1735a9aa1618c
Singularis <singularis@volny.cz>
2022-08-15 18:49:24 +0200

    [bushy_leaves] řešení problémů s výkonem vykreslování

--- mods/bushy_leaves/init.lua
+++ mods/bushy_leaves/init.lua
@@ -17,9 +17,9 @@ Codezeilen und schreibe das auch nicht dran.
 
 print("[MOD BEGIN] " .. minetest.get_current_modname() .. "(" .. os.clock() .. ")")
 
--- 0 => do not change
--- 1 => simple model
--- 2 => full bushy model
+-- 0 => no model change (only make leaves climbable, non-walkable and waving)
+-- 1 => use a cheap model
+-- 2 => use the full model
 
 local leaves_nodes = {
 	-- Cool Trees
@@ -30,7 +30,7 @@ local leaves_nodes = {
 	["clementinetree:leaves"] = 1,
 	["ebony:leaves"] = 1,
 	["hollytree:leaves"] = 1,
-	["cherrytree:leaves"] = 2,
+	["cherrytree:leaves"] = 1,
 	["chestnuttree:leaves"] = 1,
 	["jacaranda:blossom_leaves"] = 1,
 	["larch:leaves"] = 1,
@@ -45,7 +45,7 @@ local leaves_nodes = {
 	["willow:leaves"] = 1,
 
 	-- More Trees
-	["moretrees:apple_tree_leaves"] = 2,
+	["moretrees:apple_tree_leaves"] = 1,
 	["moretrees:beech_leaves"] = 1,
 	["moretrees:birch_leaves"] = 1,
 	["moretrees:cedar_leaves"] = 1,
@@ -66,46 +66,14 @@ local leaves_nodes = {
 	["default:acacia_bush_leaves"] = 1,
 	["default:acacia_leaves"] = 1,
 	["default:aspen_leaves"] = 1,
-	["default:blueberry_bush_leaves"] = 1,
+	["default:blueberry_bush_leaves"] = 2,
 	["default:blueberry_bush_leaves_with_berries"] = 2,
-	["default:bush_leaves"] = 1,
+	["default:bush_leaves"] = 2,
 	["default:leaves"] = 1,
 
 	["default:jungleleaves"] = 1,
-	["default:pine_bush_needles"] = 1,
+	["default:pine_bush_needles"] = 2,
 	["default:pine_needles"] = 1,
-}
-
-local node_box_bushy_leaves = {
-	type = "fixed",
-	fixed = {
-		{  0/16, -4/16,-12/16,  0/16,  4/16, 12/16 },
-		{  0/16, -8/16, -8/16,  0/16,  8/16,  8/16 },
-		{  0/16,-12/16, -4/16,  0/16, 12/16,  4/16 },
-
-		{-12/16,  0/16, -4/16, 12/16,  0/16,  4/16 },
-		{ -8/16,  0/16, -8/16,  8/16,  0/16,  8/16 },
-		{ -4/16,  0/16,-12/16,  4/16,  0/16, 12/16 },
-
-		{-12/16, -4/16,  0/16, 12/16,  4/16,  0/16 },
-		{ -8/16, -8/16,  0/16,  8/16,  8/16,  0/16 },
-		{ -4/16,-12/16,  0/16,  4/16, 12/16,  0/16 },
-	},
-}
-
-local node_box_bushy_leaves_low_budget = {
-	type = "fixed",
-	fixed = {
-		{  0/16, -4/16,-12/16,  0/16,  4/16, 12/16 },
-		{  0/16, -8/16, -8/16,  0/16,  8/16,  8/16 },
-		-- {  0/16,-12/16, -4/16,  0/16, 12/16,  4/16 },
-
-		{ -8/16,  0/16, -8/16,  8/16,  0/16,  8/16 },
-
-		{-12/16, -4/16,  0/16, 12/16,  4/16,  0/16 },
-		{ -8/16, -8/16,  0/16,  8/16,  8/16,  0/16 },
-		-- { -4/16,-12/16,  0/16,  4/16, 12/16,  0/16 },
-	},
 }
 
 local node_box_full_node = {
@@ -113,34 +81,19 @@ local node_box_full_node = {
 	fixed = { -8/16, -8/16, -8/16,  8/16,  8/16,  8/16 },
 }
 
---[[
-local bushy_leaves_override = {
-	drawtype = "nodebox",
-	node_box = node_box_bushy_leaves,
-	collision_box = node_box_full_node,
-	use_texture_alpha = "clip",
-} ]]
-
 local override_0 = {
 	climbable = true,
 	walkable = false,
+	waving = 2,
+	move_resistance = 1,
+	collision_box = node_box_full_node,
 }
-local override_1 = {
-	climbable = true,
-	walkable = false,
-	drawtype = "nodebox",
-	node_box = node_box_bushy_leaves_low_budget,
--- 	collision_box = node_box_full_node,
-	use_texture_alpha = "clip",
-}
-local override_2 = {
-	climbable = true,
-	walkable = false,
-	drawtype = "nodebox",
-	node_box = node_box_bushy_leaves,
---	collision_box = node_box_full_node,
-	use_texture_alpha = "clip",
-}
+local override_1 = table.copy(override_0)
+override_1.drawtype = "mesh"
+override_1.mesh = "bushy_leaves_cheap_model.obj"
+override_1.use_texture_alpha = "clip"
+local override_2 = table.copy(override_1)
+override_2.mesh = "bushy_leaves_full_model.obj"
 
 for name, level in pairs(leaves_nodes) do
 	local def = minetest.registered_nodes[name]
@@ -156,17 +109,4 @@ for name, level in pairs(leaves_nodes) do
 	end
 end
 
---[[
-local add_bushy_leaves = function()
-	-- local counter = 0
-	for i, v in ipairs(leaves_nodes) do
-		if minetest.registered_nodes[v] then
-			minetest.override_item(v, bushy_leaves_override)
-		end
-	end
-	-- print(string.format("[MOD] Bushy Leaves: %d items redefined.\n", counter))
-end
-
-minetest.register_on_mods_loaded(add_bushy_leaves)
-]]
 print("[MOD END] " .. minetest.get_current_modname() .. "(" .. os.clock() .. ")")
