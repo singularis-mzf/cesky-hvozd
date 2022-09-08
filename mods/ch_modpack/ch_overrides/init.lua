16546378063c92cc4ac992f680ed369573783349
Singularis <singularis@volny.cz>
2022-09-08 15:41:11 +0200

    [ch_overrides] lepší model pro melouny a dýně

--- mods/ch_modpack/ch_overrides/init.lua
+++ mods/ch_modpack/ch_overrides/init.lua
@@ -89,6 +89,21 @@ if minetest.get_modpath("moreblocks") and minetest.get_modpath("technic") then
 	})
 end
 
+if minetest.get_modpath("farming") and minetest.get_modpath("technic_cnc") then
+	minetest.override_item("farming:melon_8", {
+		drawtype = "mesh",
+		mesh = "technic_cnc_oblate_spheroid.obj",
+		tiles = {name = "farming_melon_side.png", backface_culling = true},
+		paramtype2 = "facedir",
+	})
+	minetest.override_item("farming:pumpkin_8", {
+		drawtype = "mesh",
+		mesh = "technic_cnc_oblate_spheroid.obj",
+		tiles = {name = "farming_pumpkin_side.png", backface_culling = true},
+		paramtype2 = "facedir",
+	})
+end
+
 dofile(modpath.."/extra_recipes.lua")
 dofile(modpath.."/falling_nodes.lua")
 dofile(modpath.."/ores.lua")
