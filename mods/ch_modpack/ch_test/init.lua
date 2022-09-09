6e16088d1967c908e6a6bda88acbebc67cd770e5
Singularis <singularis@volny.cz>
2022-09-09 06:39:38 +0200

    [ch_test] test sledování inventářů hráčských postav

--- mods/ch_modpack/ch_test/init.lua
+++ mods/ch_modpack/ch_test/init.lua
@@ -203,5 +203,27 @@ end
 
 minetest.register_on_mods_loaded(on_mods_loaded)
 
+local function on_player_inventory_action(player, action, inventory, inventory_info)
+	local message = "player_inventory_action("..player:get_player_name().." @ "..minetest.pos_to_string(vector.round(player:get_pos())).."): "
+	if action == "move" then
+		local destination_stack = inventory:get_stack(inventory_info.to_list, inventory_info.to_index)
+		message = message.."move "..inventory_info.count.." of "..destination_stack:get_name().." from "..inventory_info.from_list.."/"..inventory_info.from_index.." to "..inventory_info.to_list.."/"..inventory_info.to_index
+	elseif action == "put" or action == "take" then
+		if action == "put" then
+			message = message.." put to "
+		else
+			message = message.." take from "
+		end
+		
+		message = message..inventory_info.listname.."/"..inventory_info.index.." ("..inventory_info.stack:get_count().." of "..inventory_info.stack:get_name()..")"
+	else
+		message = message..action
+	end
+
+	minetest.log("action", message)
+end
+
+minetest.register_on_player_inventory_action(on_player_inventory_action)
+
 print("[MOD END] " .. minetest.get_current_modname() .. "(" .. os.clock() .. ")")
 
