5e0467fa17ca2dcc155cb910605f92b43cbb7cd9
Singularis <singularis@volny.cz>
2022-09-12 18:51:19 +0200

    [bushy_leaves] přidána podpora více modelů

--- mods/bushy_leaves/init.lua
+++ mods/bushy_leaves/init.lua
@@ -2,6 +2,7 @@
 
 bushy_leaves – Minetest mod to render leaves bushy
 Copyright © 2022  Nils Dagsson Moskopp (erlehmann)
+Copyright © 2022  Singularis <singularis@volny.cz>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
@@ -17,63 +18,70 @@ Codezeilen und schreibe das auch nicht dran.
 
 print("[MOD BEGIN] " .. minetest.get_current_modname() .. "(" .. os.clock() .. ")")
 
--- 0 => no model change (only make leaves climbable, non-walkable and waving)
--- 1 => use a cheap model
--- 2 => use the full model
+-- supported programs:
+--		"none" => no model change (but is make leaves climbable, non-walkable and waving)
+--		"plantlike" => use drawtype = "plantlike"
+--		"cheap" => use drawtype = "mesh" and a cheap bushy model
+--		"bushy" => use drawtype = "mesh" and a full bushy model
 
 local leaves_nodes = {
 	-- Cool Trees
-	["baldcypress:leaves"] = 1,
-	["bamboo:leaves"] = 1,
-	["birch:leaves"] = 1,
-	["cacaotree:leaves"] = 1,
-	["clementinetree:leaves"] = 1,
-	["ebony:leaves"] = 1,
-	["hollytree:leaves"] = 1,
-	["cherrytree:leaves"] = 1,
-	["chestnuttree:leaves"] = 1,
-	["jacaranda:blossom_leaves"] = 1,
-	["larch:leaves"] = 1,
-	["lemontree:leaves"] = 1,
-	["mahogany:leaves"] = 1,
-	["maple:leaves"] = 1,
-	["oak:leaves"] = 1,
-	["palm:leaves"] = 1,
-	["plumtree:leaves"] = 1,
-	["pomegranate:leaves"] = 1,
-	["sequoia:leaves"] = 1,
-	["willow:leaves"] = 1,
+	["baldcypress:leaves"] = "cheap",
+	["bamboo:leaves"] = "cheap",
+	["birch:leaves"] = "cheap",
+	["cacaotree:leaves"] = "cheap",
+	["cherrytree:leaves"] = "cheap",
+	["cherrytree:blossom_leaves"] = "plantlike",
+	["chestnuttree:leaves"] = "cheap",
+	["clementinetree:leaves"] = "cheap",
+	["darkage:dry_leaves"] = "cheap",
+	["ebony:leaves"] = "cheap",
+	["hollytree:leaves"] = "cheap",
+	["jacaranda:blossom_leaves"] = "cheap",
+	["larch:leaves"] = "cheap",
+	["lemontree:leaves"] = "cheap",
+	["mahogany:leaves"] = "cheap",
+	["maple:leaves"] = "cheap",
+	["oak:leaves"] = "cheap",
+	["palm:leaves"] = "cheap",
+	["plumtree:leaves"] = "cheap",
+	["pomegranate:leaves"] = "cheap",
+	["sequoia:leaves"] = "cheap",
+	["willow:leaves"] = "plantlike",
 
 	-- More Trees
-	["moretrees:apple_tree_leaves"] = 1,
-	["moretrees:beech_leaves"] = 1,
-	["moretrees:birch_leaves"] = 1,
-	["moretrees:cedar_leaves"] = 1,
-	["moretrees:date_palm_leaves"] = 1,
-	["moretrees:fir_leaves"] = 1,
-	["moretrees:fir_leaves_bright"] = 1,
-	["moretrees:oak_leaves"] = 1,
-	["moretrees:palm_leaves"] = 1,
-	["moretrees:poplar_leaves"] = 1,
-	["moretrees:sequoia_leaves"] = 1,
-	["moretrees:spruce_leaves"] = 1,
-	["moretrees:willow_leaves"] = 1,
-	["moretrees:rubber_tree_leaves"] = 1,
-	["moretrees:jungletree_leaves_red"] = 1,
-	["moretrees:jungletree_leaves_yellow"] = 1,
+	["moretrees:apple_tree_leaves"] = "cheap",
+	["moretrees:beech_leaves"] = "cheap",
+	["moretrees:birch_leaves"] = "cheap",
+	["moretrees:cedar_leaves"] = "cheap",
+	["moretrees:date_palm_leaves"] = "cheap",
+	["moretrees:fir_leaves"] = "cheap",
+	["moretrees:fir_leaves_bright"] = "cheap",
+	["moretrees:oak_leaves"] = "cheap",
+	["moretrees:palm_leaves"] = "cheap",
+	["moretrees:poplar_leaves"] = "cheap",
+	["moretrees:sequoia_leaves"] = "cheap",
+	["moretrees:spruce_leaves"] = "cheap",
+	["moretrees:willow_leaves"] = "cheap",
+	["moretrees:rubber_tree_leaves"] = "cheap",
+	["moretrees:jungletree_leaves_red"] = "cheap",
+	["moretrees:jungletree_leaves_yellow"] = "cheap",
+
+	-- Ethereal
+	["ethereal:bananaleaves"] = "plantlike",
 
 	-- default
-	["default:acacia_bush_leaves"] = 1,
-	["default:acacia_leaves"] = 1,
-	["default:aspen_leaves"] = 1,
-	["default:blueberry_bush_leaves"] = 2,
-	["default:blueberry_bush_leaves_with_berries"] = 2,
-	["default:bush_leaves"] = 2,
-	["default:leaves"] = 1,
+	["default:acacia_bush_leaves"] = "plantlike",
+	["default:acacia_leaves"] = "plantlike",
+	["default:aspen_leaves"] = "cheap",
+	["default:blueberry_bush_leaves"] = "bushy",
+	["default:blueberry_bush_leaves_with_berries"] = "bushy",
+	["default:bush_leaves"] = "bushy",
+	["default:leaves"] = "cheap",
 
-	["default:jungleleaves"] = 1,
-	["default:pine_bush_needles"] = 2,
-	["default:pine_needles"] = 1,
+	["default:jungleleaves"] = "cheap",
+	["default:pine_bush_needles"] = "bushy",
+	["default:pine_needles"] = "cheap",
 }
 
 local node_box_full_node = {
@@ -81,31 +89,43 @@ local node_box_full_node = {
 	fixed = { -8/16, -8/16, -8/16,  8/16,  8/16,  8/16 },
 }
 
-local override_0 = {
+local programs = {}
+
+-- "none" => no model change (but is make leaves climbable, non-walkable and waving)
+programs.none = {
 	climbable = true,
 	walkable = false,
 	waving = 2,
 	move_resistance = 1,
 	collision_box = node_box_full_node,
 }
-local override_1 = table.copy(override_0)
-override_1.drawtype = "mesh"
-override_1.mesh = "bushy_leaves_cheap_model.obj"
-override_1.use_texture_alpha = "clip"
-local override_2 = table.copy(override_1)
-override_2.mesh = "bushy_leaves_full_model.obj"
 
-for name, level in pairs(leaves_nodes) do
+-- "plantlike" => use drawtype = "plantlike"
+local p = table.copy(programs.none)
+p.drawtype = "plantlike"
+p.visual_scale = 1.4
+programs.plantlike = p
+
+-- "cheap" => use drawtype = "mesh" and a cheap bushy model
+p = table.copy(programs.none)
+p.drawtype = "mesh"
+p.mesh = "bushy_leaves_cheap_model.obj"
+p.use_texture_alpha = "clip"
+programs.cheap = p
+
+-- "bushy" => use drawtype = "mesh" and a full bushy model
+p = table.copy(p)
+p.mesh = "bushy_leaves_full_model.obj"
+programs.bushy = p
+
+for name, program_name in pairs(leaves_nodes) do
 	local def = minetest.registered_nodes[name]
+	local program = programs[program_name]
 	if def then
-		def.walkable = false
-		if level == 2 then
-			minetest.override_item(name, override_2)
-		elseif level == 1 then
-			minetest.override_item(name, override_1)
-		else
-			minetest.override_item(name, override_0)
+		if not program then
+			error("Invalid program: "..program_name)
 		end
+		minetest.override_item(name, program)
 	end
 end
 
