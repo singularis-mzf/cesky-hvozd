d1c7df9327da4102fc16e4a94298573d349a581e
Singularis <singularis@volny.cz>
2022-09-12 15:36:43 +0200

    [mail_mod] pročištění kódu

--- mods/mail_mod/util/normalize.lua
+++ mods/mail_mod/util/normalize.lua
@@ -11,32 +11,9 @@ function mail.normalize_players_and_add_recipients(field, recipients)
 end
 
 function mail.parse_player_list(field)
-	error("DEPRECATED") --[[
-    if not field then
-        return {}
-    end
-
-    local separator = ", "
-    local pattern = "([^" .. separator .. "]+)"
-
-    -- get individual players
-    local player_set = {}
-    local order = {}
-    field:gsub(pattern, function(c)
-        if player_set[string.lower(c)] == nil then
-            player_set[string.lower(c)] = c
-            order[#order+1] = c
-        end
-    end)
-
-    return order ]]
-end
-
-function mail.concat_player_list(order)
 	error("DEPRECATED")
-    --[[ turn list of players back into normalized string
-    return table.concat(order, ", ") ]]
 end
+mail.concat_player_list = mail.parse_player_list
 
 --[[
 	string login_name -- a canonical login name; the player is not required to exist
@@ -78,36 +55,31 @@ function mail.player_list_to_login_names(player_list, options)
 		skip_set = {}
 	end
 
-	-- print("DEBUG: will split list: "..player_list)
 	local parts = string.split(player_list, "[,;|&]", false, -1, true)
 	local result = {}
 	for _, part in ipairs(parts) do
 		local name_in_the_list = part:trim()
 		if name_in_the_list ~= "" then
 			local login_name = ch_core.jmeno_na_prihlasovaci(name_in_the_list)
-			if skip_set[login_name] then
-				-- print("- will skip "..name_in_the_list..", because "..login_name.." is in the skip_set")
-			elseif minetest.player_exists(login_name) then
+			if not skip_set[login_name] then
 				skip_set[login_name] = 1
-				table.insert(result, login_name)
-				-- print("- add "..login_name.." to the resulting login names (and to the skip set)")
-			else
-				table.insert(not_found_list, name_in_the_list)
-				-- print("- add "..name_in_the_list.." to the not_found_list (and to the skip set)")
-				skip_set[login_name] = 1
-
-				if options.pass_not_found then
-					table.insert(result, name_in_the_list)
+				if minetest.player_exists(login_name) then
+					table.insert(result, login_name)
+				else
+					table.insert(not_found_list, name_in_the_list)
+					if options.pass_not_found then
+						table.insert(result, name_in_the_list)
+					end
 				end
 			end
 		end
 	end
-	-- print("finished with "..#result.." resulting login names")
 	return result
 end
 
 --[[
 	options:
+		table skip_set
 		bool use_login_names
 ]]
 
@@ -123,46 +95,20 @@ function mail.login_names_to_player_list(login_names, options)
 	end
 
 	local result = {}
-	local is_first = true
 
-	print("mail.login_names_to_player_list() started")
 	for _, name in ipairs(login_names) do
-		if skip_set[name] then
-			print("- will skip "..login_name..", because it is in the skip_set")
-		else
-			if not is_first then
-				table.insert(result, ", ")
-			else
-				is_first = false
-			end
-			local name_to_add
+		if not skip_set[name] then
 			if options.use_login_names then
-				name_to_add = name
+				table.insert(result, name)
 			else
-				name_to_add = ch_core.prihlasovaci_na_zobrazovaci(name)
+				table.insert(result, ch_core.prihlasovaci_na_zobrazovaci(name))
 			end
-			print("- will add "..name_to_add.." from login name "..name)
-			table.insert(result, name_to_add)
 		end
 	end
 
-	result = table.concat(result)
-	print(">> list "..result)
-	return result
+	return table.concat(result, ", ")
 end
 
 function mail.login_names_to_login_list(login_names)
-	local result = {}
-	local is_first = true
-
-	for _, name in ipairs(login_names) do
-		if not is_first then
-			table.insert(result, ", ")
-		else
-			is_first = false
-		end
-		table.insert(result, name)
-	end
-
-	return table.concat(result)
+	return table.concat(login_names, ", ")
 end
