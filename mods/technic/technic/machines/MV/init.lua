5a89dd0ce173807951d934bddacde88832e8223b
Singularis <singularis@volny.cz>
2022-10-30 06:05:37 +0100

    [technic] přidány administrátorské generátory

--- mods/technic/technic/machines/MV/init.lua
+++ mods/technic/technic/machines/MV/init.lua
@@ -14,6 +14,7 @@ end
 dofile(path.."/generator.lua")
 dofile(path.."/solar_array.lua")
 dofile(path.."/hydro_turbine.lua")
+dofile(path.."/admin_generator.lua")
 
 -- Machines
 dofile(path.."/alloy_furnace.lua")
