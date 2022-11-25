0ee855152c7763513843bffa53f99cf82ae5c917
Singularis <singularis@volny.cz>
2022-11-25 17:03:21 +0100

    [paintings] přizpůsobení módu

--- mods/paintings/init.lua
+++ mods/paintings/init.lua
@@ -3,51 +3,49 @@
 
 local paintings = {}
 paintings.dyes = {
-	{"white",      "White"},
-	{"grey",       "Grey"},
-	{"black",      "Black"},
-	{"red",        "Red"},
-	{"yellow",     "Yellow"},
-	{"green",      "Green"},
-	{"cyan",       "Cyan"},
-	{"blue",       "Blue"},
-	{"magenta",    "Magenta"},
-	{"orange",     "Orange",},
-	{"violet",     "Violet"},
-	{"brown",      "Brown"},
-	{"pink",       "Pink"},
-	{"dark_grey",  "Dark Grey"},
-	{"dark_green", "Dark Green"},
+	{"white",      "White", "dekorativní malba 21: Sněžné údolí"},
+	{"grey",       "Grey", "dekorativní malba 29: Horské jezero"},
+	{"black",      "Black", "dekorativní malba 22: Na Blatech"},
+	{"red",        "Red", "dekorativní malba 23: Vulkán"},
+	{"yellow",     "Yellow", "dekorativní malba 34: Cesta"},
+	{"green",      "Green", "dekorativní malba 33: Horská stezka"},
+	{"cyan",       "Cyan", "dekorativní malba 32: Na horách"},
+	{"blue",       "Blue", "dekorativní malba 26: Pláž"},
+	{"magenta",    "Magenta", "dekorativní malba 28: Oáza"},
+	{"orange",     "Orange", "dekorativní malba 27: Útesy pouště"},
+	{"violet",     "Violet", "dekorativní malba 24: Fialová fantazie"},
+	{"brown",      "Brown", "dekorativní malba 25: Podzimní louka"},
+	{"pink",       "Pink", "dekorativní malba 28: Západ/východ slunce 2"},
+	{"dark_grey",  "Dark Grey", "dekorativní malba 30: Tajuplný les"},
+	{"dark_green", "Dark Green", "dekorativní malba 31: Močál"},
 }
+
+local selection_box = minetest.registered_nodes["homedecor:painting_1"].selection_box
 
 for _, row in ipairs(paintings.dyes) do
 	local name = row[1]
 	local desc = row[2]
 	minetest.register_node("paintings:" .. name, {
-		description = desc .. " Painting",
-		drawtype = "nodebox",
-		tiles = {"paintings_" .. name .. ".png"},
-		inventory_image = "paintings_" .. name .. ".png",
-		wield_image = "paintings_" .. name .. ".png",
+		description = row[3],
+		drawtype = "mesh",
+		mesh = "homedecor_painting.obj",
+		tiles = {"default_wood.png", "homedecor_blank_canvas.png", "paintings_" .. name .. ".png"},
+		--[[ inventory_image = "paintings_" .. name .. ".png",
+		wield_image = "paintings_" .. name .. ".png", ]]
 		paramtype = "light",
-		paramtype2 = "wallmounted",
+		paramtype2 = "facedir",
 		sunlight_propagates = true,
 		walkable = false,
-		node_box = {
-			type = "wallmounted",
-			wall_top    = {-0.5, 0.4375, -0.5, 0.5, 0.5, 0.5},
-			wall_bottom = {-0.5, -0.5, -0.5, 0.5, -0.4375, 0.5},
-			wall_side   = {-0.5, -0.5, -0.5, -0.4375, 0.5, 0.5},
-		},
+		selection_box = selection_box,
 		groups = {snappy = 2, choppy = 2, oddly_breakable_by_hand = 3, flammable = 3},
 		sounds = default.node_sound_wood_defaults(),
 	})
 	minetest.register_craft({
 		output = "paintings:" .. name,
 		recipe = {
-			{"group:stick", "group:stick", "group:stick"},
-			{"group:stick", "wool:" .. name, "group:stick"},
-			{"group:stick", "group:stick", "group:stick"},
+			{"group:stick", "", ""},
+			{"dye:"..name, "", ""},
+			{"homedecor:blank_canvas", "", ""},
 		}
 	})
 end
