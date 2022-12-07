976f06b9d1b9d1c522a995a840fc8c0d333b7cb0
Singularis <singularis@volny.cz>
2022-12-07 19:33:25 +0100

    [ch_extras] přidán experimentální šumový blok

--- mods/ch_modpack/ch_extras/nodes.lua
+++ mods/ch_modpack/ch_extras/nodes.lua
@@ -459,6 +459,34 @@ ch_core.register_nodes(
 		}
 	})
 
+-- ch_extras:noise
+---------------------------------------------------------------
+def = {
+	drawtype = "normal",
+	description = "šumový blok [EXPERIMENTÁLNÍ]",
+	_ch_help = "Tento blok je experimentální. Můžete ho zkoušet, ale nepoužívejte ho bez domluvy na vážně míněných stavbách.",
+	tiles = {{name = "ch_extras_noise.png", animation = {
+		type = "vertical_frames",
+		aspect_w = 64,
+		aspect_h = 64,
+		length = 0.25,
+	}}},
+	paramtype = "light",
+	paramtype2 = "color",
+	palette = "unifieddyes_palette_extended.png",
+	light_source = 5,
+	groups = {dig_immediate = 2, ud_param2_colorable = 1},
+	sounds = default.node_sound_defaults(),
+	is_ground_content = false,
+}
+
+if minetest.get_modpath("unifieddyes") then
+	def.on_construct = unifieddyes.on_construct
+	def.on_dig = unifieddyes.on_dig
+end
+
+minetest.register_node("ch_extras:noise", def)
+
 --[[ PLAKÁTY
 local box = {
 		type = "fixed",
