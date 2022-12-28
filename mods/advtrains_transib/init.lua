e44d581abd317fe91729afec573795130296bfa0
Singularis <singularis@volny.cz>
2022-12-28 11:24:10 +0100

    [advtrains_transib] adaptace a lokalizace

--- mods/advtrains_transib/init.lua
+++ mods/advtrains_transib/init.lua
@@ -1,13 +1,9 @@
-local S
-if minetest.get_modpath("intllib") then
-    S = intllib.Getter()
-else
-    S = function(s,a,...)a={a,...}return s:gsub("@(%d+)",function(n)return a[tonumber(n)]end)end
-end
+local S = attrans
 
 advtrains.register_wagon("engine_transib", {
 	mesh="advtrains_engine_transib.b3d",
 	textures = {"advtrains_engine_transib.png"},
+	backface_culling = false,
 	drives_on={default=true},
 	max_speed=20,
 	seats = {
@@ -45,12 +41,12 @@ advtrains.register_wagon("engine_transib", {
 	},
 	seat_groups = {
 		dstand={
-			name = "Driver Stand",
+			name = S("Driver Stand"),
 			access_to = {"pass"},
 			require_doors_open=true,
 		},
 		pass={
-			name = "Passenger area",
+			name = S("Passenger area"),
 			access_to = {"dstand"},
 			require_doors_open=true,
 		},
@@ -71,8 +67,14 @@ advtrains.register_wagon("engine_transib", {
 	wagon_span=3.4,
 	is_locomotive=true,
 	collisionbox = {-1.0,-0.5,-1.0, 1.0,2.5,1.0},
-	drops={"advtrains:engine_transib"},
-}, S("transib "), "advtrains_engine_transib_inv.png")
+	drops={
+		"wool:blue 2",
+		"advtrains:driver_cab",
+		"default:steelblock 2",
+		"basic_materials:motor",
+		"advtrains:wheel 3",
+	},
+}, S("transib"), "advtrains_engine_transib_inv.png")
 
 advtrains.register_wagon("wagon_coal", {
 	mesh="advtrains_wagon_coal.b3d",
@@ -83,10 +85,35 @@ advtrains.register_wagon("wagon_coal", {
 	visual_size = {x=1, y=1},
 	wagon_span=2.2,
 	collisionbox = {-1.0,-0.5,-1.0, 1.0,2.5,1.0},
-	drops={"advtrains:wagon_coal"},
+	drops={
+		"default:steelblock 5",
+		"dye:brown",
+		"advtrains:wheel 2",
+	},
 	has_inventory = true,
 	get_inventory_formspec = advtrains.standard_inventory_formspec,
 	inventory_list_sizes = {
 		box=8*6,
 	},
 }, S("Coal Wagon(for transib)"), "advtrains_wagon_coal_inv.png")
+
+minetest.register_craft({
+	output = "advtrains:engine_transib",
+	recipe = {
+		{"wool:blue", "advtrains:driver_cab", "wool:blue"},
+		{"default:steelblock", "basic_materials:motor", "default:steelblock"},
+		{"advtrains:wheel", "advtrains:wheel", "advtrains:wheel"},
+	},
+})
+
+
+minetest.register_craft({
+	output = "advtrains:wagon_coal",
+	recipe = {
+		{"default:steelblock", "dye:brown", "default:steelblock"},
+		{"default:steelblock", "", "default:steelblock"},
+		{"advtrains:wheel", "default:steelblock", "advtrains:wheel"},
+	},
+})
+
+
