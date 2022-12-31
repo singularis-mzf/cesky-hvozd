7e83b411c860cc452c0c9184b5b3edc55e8978b0
Singularis <singularis@volny.cz>
2022-12-31 20:50:27 +0100

    [technic_hv_extend] přidána VN slévací pec

--- mods/technic_hv_extend/init.lua
+++ mods/technic_hv_extend/init.lua
@@ -2,6 +2,7 @@ print("[MOD BEGIN] " .. minetest.get_current_modname() .. "(" .. os.clock() .. "
 local modname = minetest.get_current_modname()
 local path = minetest.get_modpath(modname)
 
+dofile(path.."/alloy_furnace.lua")
 dofile(path.."/electric_furnace.lua")
 dofile(path.."/grinder.lua")
 dofile(path.."/compressor.lua")
