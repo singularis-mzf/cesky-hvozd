557247c86c20cbd70c843e31ba54775491a716cd
Singularis <singularis@volny.cz>
2023-01-13 07:58:29 +0100

    [ch_overrides] patinu z mědi lze odstranit sekyrou
    - [ch_decor] přidány čtyři skladby pro NN rozhlasový přijímač

--- mods/ch_modpack/ch_overrides/mod_overrides/moreblocks.lua
+++ mods/ch_modpack/ch_overrides/mod_overrides/moreblocks.lua
@@ -26,3 +26,45 @@ for _, recipeitem in ipairs(stairsplus:get_recipeitems()) do
 		minetest.override_item(itemname, override)
 	end
 end
+
+-- REMOVE PATINA BY AXE
+local function on_punch(pos, node, puncher, pointed_thing)
+	local player_name = puncher and puncher:get_player_name()
+	if player_name == nil or player_name == "" then
+		return
+	end
+	local wielded_item = puncher:get_wielded_item()
+	if wielded_item:is_empty() or minetest.get_item_group(wielded_item:get_name(), "axe") == 0 then
+		return
+	end
+	if minetest.is_protected(pos, player_name) then
+		minetest.record_protection_violation(pos, player_name)
+		return
+	end
+	if node.name == "moreblocks:copperpatina" then
+		node.name = "default:copperblock"
+	else
+		local recipeitem, category, alternate = stairsplus:analyze_shape(node.name)
+		if recipeitem == nil then
+			minetest.log("error", "Shape analysis of "..node.name.." failed!")
+		end
+		local new_node_name = stairsplus:get_shape("default:copperblock", category, alternate)
+		if new_node_name == nil then
+			minetest.log("warning", "Expected shape "..category.."/"..alternate.." of default:copperblock not found!")
+			return
+		end
+		node.name = new_node_name
+	end
+	minetest.swap_node(pos, node)
+	minetest.sound_play({name = "default_dig_metal", gain = 0.5}, {pos = pos}, true)
+end
+
+override = {on_punch = on_punch}
+minetest.override_item("moreblocks:copperpatina", override)
+for _, shapedef in ipairs(stairsplus.shapes_list) do
+	local category, alternate = shapedef[1]:sub(1,-2), shapedef[2]
+	local item = stairsplus:get_shape("moreblocks:copperpatina", category, alternate)
+	if item ~= nil then
+		minetest.override_item(item, override)
+	end
+end
