3030de970b15fedbf179edfa064007320162350c
Singularis <singularis@volny.cz>
2023-02-09 17:02:50 +0100

    [ch_overrides] zemina je nově  mírně pod úrovní terénu

--- mods/ch_modpack/ch_overrides/mod_overrides/farming.lua
+++ mods/ch_modpack/ch_overrides/mod_overrides/farming.lua
@@ -1,3 +1,30 @@
+local def = {
+	drawtype = "nodebox",
+	node_box = {
+		type = "fixed",
+		fixed = {-0.5, -0.5, -0.5, 0.5, 0.5 - 2 / 32, 0.5},
+	},
+	paramtype = "light",
+	paramtype2 = "facedir",
+}
+
+if minetest.get_modpath("screwdriver") then
+	def.on_rotate = screwdriver.rotate_simple
+end
+
+for _, name in ipairs({"dry_soil", "soil"}) do
+	minetest.override_item("farming:"..name, def)
+end
+
+def.node_box = {
+	type = "fixed",
+	fixed = {-0.5, -0.5, -0.5, 0.5, 0.5 - 3 / 32, 0.5},
+}
+for _, name in ipairs({"dry_soil_wet", "soil_wet"}) do
+	minetest.override_item("farming:"..name, def)
+end
+
+
 if minetest.get_modpath("technic_cnc") then
 	minetest.override_item("farming:melon_8", {
 		drawtype = "mesh",
