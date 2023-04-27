18503d6f0ec5a7e1c0da5625d5054e3ef7a2e6d3
Singularis <singularis@volny.cz>
2023-04-27 06:07:18 +0200

    [ch_extras] přidány luxusní obsidiánové dveře, ...
    - luxusní prosklené dveře
    - stylové dřevěné dveře

--- mods/ch_modpack/ch_extras/nodes.lua
+++ mods/ch_modpack/ch_extras/nodes.lua
@@ -736,6 +736,64 @@ for _, chest in ipairs({"default:chest", "default:chest_locked"}) do
 	})
 end
 
+-- Doors: ch_extras:*
+---------------------------------------------------------------
+def = {
+	tiles = {{name = "ch_extras_obsidian_door.png", backface_culling = true}},
+	description = "luxusní obsidiánové dveře",
+	inventory_image = "ch_extras_obsidian_door_item.png",
+	groups = {node = 1, cracky = 3},
+	sounds = default.node_sound_glass_defaults(),
+	sound_open = "doors_glass_door_open",
+	sound_close = "doors_glass_door_close",
+	gain_open = 0.3, gain_close = 0.25,
+	recipe = {
+		{"default:gold_ingot", ""},
+		{"doors:door_obsidian_glass", ""},
+	},
+}
+doors.register("door_luxury_obsidian", def)
+minetest.register_craft({
+	output = "doors:door_luxury_obsidian",
+	recipe = {
+		{"default:obsidian_glass", "default:obsidian_glass", "default:gold_ingot"},
+		{"default:obsidian_glass", "default:obsidian_glass", ""},
+		{"default:obsidian_glass", "default:obsidian_glass", ""},
+	},
+})
+
+def = {
+	tiles = {{name = "ch_extras_glass_door.png", backface_culling = true}},
+	description = "luxusní prosklené dveře",
+	inventory_image = "ch_extras_glass_door_item.png",
+	groups = {node = 1, cracky = 3},
+	sounds = default.node_sound_glass_defaults(),
+	sound_open = "doors_glass_door_open",
+	sound_close = "doors_glass_door_close",
+	gain_open = 0.3, gain_close = 0.25,
+	recipe = {
+		{"default:glass", "default:steel_ingot", ""},
+		{"default:glass", "default:glass", ""},
+		{"default:glass", "group:wood", ""},
+	},
+}
+doors.register("door_luxury_glass", def)
+
+def = {
+	tiles = {{name = "ch_extras_wood_door.png", backface_culling = true}},
+	description = "stylové dřevěné dveře",
+	inventory_image = "ch_extras_wood_door_item.png",
+	groups = {node = 1, choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+	sounds = default.node_sound_wood_defaults(),
+	gain_open = 0.06, gain_close = 0.13,
+	recipe = {
+		{"default:glass", "default:glass", ""},
+		{"group:wood", "group:wood", ""},
+		{"group:wood", "group:wood", ""},
+	},
+}
+doors.register("door_luxury_wood", def)
+
 -- ch_extras:builder
 ---------------------------------------------------------------
 local xyz = {"x", "y", "z"}
