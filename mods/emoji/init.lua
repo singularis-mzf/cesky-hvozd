d11c9e6bb0b8469a2ee9dbb3f6ec193ec979b626
Singularis <singularis@volny.cz>
2024-02-11 11:33:08 +0100

    [emoji] adaptace módu

--- mods/emoji/init.lua
+++ mods/emoji/init.lua
@@ -1,79 +1,173 @@
-local bg = "bg_emoji.png"
-
-local form = "size[10,7.8] bgcolor[#333444cc; false] image_button_exit[0,0;2,2;"..bg.."^1_emoji.png;1_emoji;] image_button_exit[2,0;2,2;"..bg.."^2_emoji.png;2_emoji;] image_button_exit[4,0;2,2;"..bg.."^3_emoji.png;3_emoji;] image_button_exit[6,0;2,2;"..bg.."^4_emoji.png;4_emoji;] image_button_exit[8,0;2,2;"..bg.."^5_emoji.png;5_emoji;] image_button_exit[0,2;2,2;"..bg.."^6_emoji.png;6_emoji;] image_button_exit[2,2;2,2;"..bg.."^7_emoji.png;7_emoji;] image_button_exit[4,2;2,2;"..bg.."^8_emoji.png;8_emoji;] image_button_exit[6,2;2,2;"..bg.."^9_emoji.png;9_emoji;] image_button_exit[8,2;2,2;"..bg.."^10_emoji.png;10_emoji;] image_button_exit[0,4;2,2;"..bg.."^11_emoji.png;11_emoji;] image_button_exit[2,4;2,2;"..bg.."^12_emoji.png;12_emoji;] image_button_exit[4,4;2,2;"..bg.."^13_emoji.png;13_emoji;] image_button_exit[6,4;2,2;"..bg.."^14_emoji.png;14_emoji;] image_button_exit[8,4;2,2;"..bg.."^15_emoji.png;15_emoji;] image_button_exit[0,6;2,2;"..bg.."^16_emoji.png;16_emoji;] image_button_exit[2,6;2,2;"..bg.."^17_emoji.png;17_emoji;] image_button_exit[4,6;2,2;"..bg.."^18_emoji.png;18_emoji;] image_button_exit[6,6;2,2;"..bg.."^19_emoji.png;19_emoji;] image_button_exit[8,6;2,2;"..bg.."^20_emoji.png;20_emoji;]"
-
-
-minetest.register_chatcommand("e", {
-	params = "",
-	description = "Emoji",
-	privs = {},
-	func = function(name, param)
-
-		minetest.show_formspec(name, "emoji_form", form)
-	
-	end,
-})
-
-local v = {
-	{"1_emoji", ":)"},
-	{"2_emoji", "B-)"},
-	{"3_emoji", ":#"},
-	{"4_emoji", "*_*"},
-	{"5_emoji", ":D"},
-	{"6_emoji", ":*"},
-	{"7_emoji", ":_("},
-	{"8_emoji", ">:-["},
-	{"9_emoji", "]:-)"},
-	{"10_emoji", ":/"},
-	{"11_emoji", ";)"},
-	{"12_emoji", ":("},
-	{"13_emoji", ";P"},
-	{"14_emoji", ":'-D"},
-	{"15_emoji", "~:["},
-	{"16_emoji", "o_O"},
-	{"17_emoji", "xD"},
-	{"18_emoji", "xP"},
-	{"19_emoji", ":X"},
-	{"20_emoji", ":P"},
-	{"21_emoji", ":O"},
-	
+local emojis = {
+	{
+		name = "1_emoji",
+		en_name = "smile",
+		texts = {":)", ":-)"},
+	}, {
+		name = "2_emoji",
+		en_name = "sunglasses",
+		texts = {"B-)"},
+	}, {
+		name = "3_emoji",
+		en_name = "mask",
+		texts = {":#"},
+	},{
+		name = "4_emoji",
+		en_name = "love",
+		texts = {"*_*"},
+	}, {
+		name = "5_emoji",
+		en_name = "laugh",
+		texts = {":D", ":-D"},
+	}, {
+		name = "6_emoji",
+		en_name = "kiss",
+		texts = {":*", ":-*"},
+	}, {
+		name = "7_emoji",
+		en_name = "cry",
+		texts = {":_(", ":'("},
+	}, {
+		name = "8_emoji",
+		en_name = "angry",
+		texts = {">:-["},
+	}, {
+		name = "9_emoji",
+		-- en_name = "",
+		texts = {"]:-)"},
+	}, {
+		name = "10_emoji",
+		en_name = "nothing_to_say",
+		texts = {":/", ":-/"},
+	}, {
+		name = "11_emoji",
+		en_name = "wink",
+		texts = {";)", ";-)"},
+	}, {
+		name = "12_emoji",
+		en_name = "sad",
+		texts = {":(", ":-("},
+	}, {
+		name = "13_emoji",
+		en_name = "tongue",
+		texts = {";P", ";-P"},
+	}, {
+		name = "14_emoji",
+		-- en_name = "",
+		texts = {":'-D"},
+	}, {
+		name = "15_emoji",
+		-- en_name = "",
+		texts = {"~:["},
+	}, {
+		name = "16_emoji",
+		-- en_name = "",
+		texts = {"o_O"},
+	}, {
+		name = "17_emoji",
+		-- en_name = "",
+		texts = {"xD"},
+	}, {
+		name = "18_emoji",
+		-- en_name = "",
+		texts = {"xP"},
+	}, {
+		name = "19_emoji",
+		-- en_name = "",
+		texts = {":X"},
+	}, {
+		name = "20_emoji",
+		-- en_name = "",
+		texts = {":O", ":-O"},
+	}
 }
 
-minetest.register_on_player_receive_fields(function(player, formname, fields)
-	if formname=="emoji_form" then
+local name_to_def = {}
+local text_to_def = {}
 
-		local pos = player:get_pos()
-		
-		for _, v in pairs(v) do
-
-				if fields[v[1]] then
-					minetest.sound_play("emoji_sound", {pos = pos, max_hear_distance = 12, gain = 1.0,})
-
-					minetest.add_particlespawner(
-						1, --amount
-						0.01, --time
-						{x=pos.x, y=pos.y+2, z=pos.z}, --minpos
-						{x=pos.x, y=pos.y+2, z=pos.z}, --maxpos
-						{x=0, y=0.15, z=0}, --minvel
-						{x=0, y=0.15, z=0}, --maxvel
-						{x=0,y=0,z=0}, --minacc
-						{x=0,y=0,z=0}, --maxacc
-						2.5, --minexptime
-						2.5, --maxexptime
-						9, --minsize
-						9, --maxsize
-						false, --collisiondetection
-						v[1]..".png"
-					)
-
-				end	
+for _, def in ipairs(emojis) do
+	if name_to_def[def.name] ~= nil then
+		error("Emoji name conflict: "..def.name)
+	end
+	name_to_def[def.name] = def
+	for _, text in ipairs(def.texts) do
+		if text_to_def[text] ~= nil then
+			error("Emoji text conflict: "..text)
 		end
+		text_to_def[text] = def
+	end
+end
 
+local form = {
+	"formspec_version[6]",
+	"size[10.5,9.75]",
+	"bgcolor[#333444cc;false]",
+	"tableoptions[highlight=#000000;highlight_text=#ffffff]",
+	"tablecolumns[text,align=center]",
+}
+for i, def in ipairs(emojis) do
+	local x = (i - 1) % 5
+	local y = 0.5 + (i - 1 - x) / 5 * 2.25
+	x = 2.0 * x + 0.5
+	local texture = "[combine:32x32:6,6="..def.name..".png"
+	local em_space = "\xe2\x80\x83"
+	table.insert(form, "image_button_exit["..x..","..y..";1.5,1.5;"..minetest.formspec_escape(texture)..";"..def.name..";;true;true;]")
+	table.insert(form, "table["..x..","..(y + 1.5)..";1.5,0.5;;"..minetest.formspec_escape(table.concat(def.texts, em_space))..";]")
+end
+form = table.concat(form)
+
+local emoji_velocity = vector.new(0, 0.1, 0)
+local emoji_acceleration = vector.zero()
+local function show_emoji(pos, name)
+	if name_to_def[name] == nil then return end
+	minetest.sound_play("emoji_sound", {pos = pos, max_hear_distance = 12, gain = 1.0,})
+
+	minetest.add_particle{
+		pos = vector.offset(pos, 0, 2.4, 0),
+		velocity = emoji_velocity,
+		acceleration = emoji_acceleration,
+		expirationtime = 2.5,
+		collisiondetection = false,
+		texture = name..".png",
+		size = 9,
+	}
+end
+
+local def = {
+	params = "[text]",
+	description = "zobrazí nad postavou smajlíka; nezadáte-li text, vybere se smajlík pomocí formuláře a neobjeví se v četu",
+	privs = {},
+	func = function(player_name, param)
+		if param == "" then
+			minetest.show_formspec(player_name, "emoji_form", form)
+		else
+			local def = text_to_def[param]
+			local player = minetest.get_player_by_name(player_name)
+			if def ~= nil and player ~= nil then
+				local pos = player:get_pos()
+				show_emoji(pos, def.name)
+				ch_core.chat("mistni", player_name, param, pos)
+			end
+		end
+	end,
+}
+
+minetest.register_chatcommand("s", def)
+minetest.register_chatcommand("smajlík", def)
+minetest.register_chatcommand("smajlik", def)
+
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+	if formname == "emoji_form" then
+		for name, _ in pairs(fields) do
+			if name_to_def[name] ~= nil then
+				show_emoji(player:get_pos(), name)
+				return
+			end
+		end
 	end
 end)
 
-
-
+--[[
 minetest.register_on_chat_message(function(name, message, pos)    
 	local checkingmessage=( name.." "..message .." " )
 
@@ -106,9 +200,4 @@ minetest.register_on_chat_message(function(name, message, pos)
 	end	
 		
 end)
-
-
-
-
-
-
+]]
