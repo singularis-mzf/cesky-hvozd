f0b4a49ec3cae4e4f78b432b6e1771bd3ded71d9
Singularis <singularis@volny.cz>
2024-02-14 20:41:57 +0100

    [ch_core,ch_test] bezvýznamné změny

--- mods/ch_modpack/ch_test/init.lua
+++ mods/ch_modpack/ch_test/init.lua
@@ -18,5 +18,26 @@ dofile(modpath.."/wanted_blocks.lua")
 ----
 dofile(modpath.."/array_stats.lua")
 
+--[[
+local ore_nodenames = {
+	"default:stone_with_coal", "default:stone_with_iron",
+	"default:stone_with_copper", "default:stone_with_tin",
+	-- "default:stone_with_gold",
+	"default:stone_with_mese", "default:stone_with_diamond",
+	"moreores:silver_lump", "moreores:mithril_lump",
+	"denseores:large_coal_ore",
+	"denseores:large_copper_ore",
+	"denseores:large_diamond_ore",
+	-- "denseores:large_gold_ore",
+	"denseores:large_iron_ore",
+	"denseores:large_mese_ore",
+	"denseores:large_tin_ore",
+	"denseores:large_silver_ore",
+	"denseores:large_mithril_ore",
+	"technic:mineral_lead",
+-- + uranium
+}
+]]
+
 print("[MOD END] " .. minetest.get_current_modname() .. "(" .. os.clock() .. ")")
 
