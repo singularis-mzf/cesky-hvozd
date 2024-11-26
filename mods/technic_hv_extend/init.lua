f3793a65fa510b2198c96f6c55e15774c71b4320
Singularis <singularis@volny.cz>
2024-11-26 18:15:26 +0100

    [technic_hv_extend] mód odstraněn

--- mods/technic_hv_extend/init.lua
+++
@@ -1,9 +0,0 @@
-print("[MOD BEGIN] " .. minetest.get_current_modname() .. "(" .. os.clock() .. ")")
-local modname = minetest.get_current_modname()
-local path = minetest.get_modpath(modname)
-
-dofile(path.."/alloy_furnace.lua")
-dofile(path.."/electric_furnace.lua")
-dofile(path.."/grinder.lua")
-dofile(path.."/compressor.lua")
-print("[MOD END] " .. minetest.get_current_modname() .. "(" .. os.clock() .. ")")
