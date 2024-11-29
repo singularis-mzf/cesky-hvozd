9d7f8f6058b6162667a49635aaa3e19f78439173
Singularis <singularis@volny.cz>
2024-11-29 14:26:56 +0100

    [ch_overrides] implementovány skupiny podle drawtype
    - [ch_overrides,moreblocks,ch_extras] úprava toho, k jakým blokům se
      přichytávají tyče

--- mods/ch_modpack/ch_overrides/drawtype_groups.lua
+++ mods/ch_modpack/ch_overrides/drawtype_groups.lua
@@ -8,6 +8,38 @@ local cube_drawtypes = {
     plantlike_rooted = 7,
 }
 
+local function get_attracts_poles_rank(name, ndef)
+    local groups = ndef.groups or {}
+    if groups.sign then
+        if not name:match("_yard$") and not name:match("_on_pole$") then
+            return 1
+        end
+    elseif groups.display_api then
+        if
+            not name:match("^digiterms:") and
+            not name:match("^ontime_clocks:") and
+            not name:match("^signs_road:inv") -- no invisible signs
+        then
+            return 1
+        end
+    elseif groups.streets_light or groups.technic_cnc_arch216_flange or
+        groups.technic_cnc_bannerstone or groups.technic_cnc_block_fluted or
+        groups.technic_cnc_cylinder or
+        groups.technic_cnc_oct or
+        groups.technic_cnc_opposedcurvededge or
+        groups.technic_cnc_onecurvededge or
+        groups.technic_cnc_sphere or
+        groups.technic_cnc_spike or
+        groups.technic_cnc_twocurvededge or
+        groups.technic_cnc_valley
+    then
+            return 1
+    elseif name:match("^itemframes:") and name ~= "itemframes:frame_invis" then
+        return 1
+    end
+    return 0
+end
+
 local function get_full_cube_rank(ndef)
     local result = 1
     local drawtype = ndef.drawtype
@@ -33,16 +65,15 @@ local function do_override()
     end
     for _, name in ipairs(names) do
         local ndef = minetest.registered_nodes[name]
-        local rank = get_full_cube_rank(ndef)
-        -- print("DEBUG: rank for "..name.." is "..rank)
-        if rank > 0 then
-            minetest.override_item(name, {
-                groups = ch_core.assembly_groups(ndef.groups, {full_cube_node = rank})
-            })
-            count = count + 1
-        end
+        local new_groups = ch_core.assembly_groups(ndef.groups, {
+            attracts_poles = get_attracts_poles_rank(name, ndef),
+            ["drawtype_"..(ndef.drawtype or "normal")] = 1,
+            full_cube_node = get_full_cube_rank(ndef),
+        })
+        minetest.override_item(name, {groups = new_groups})
+        count = count + 1
     end
-    print("[ch_overrides/drawtype_groups] "..count.." full cube nodes group overriden ("..#names.." candidates)")
+    print("[ch_overrides/drawtype_groups] "..count.." nodes overriden")
 end
 
 do_override()
